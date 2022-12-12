9cfd6e51ac0f63bdb5326e72960715de41939822
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-12-12 16:15:04 +0800

    修改接口

--- src/main/java/com/example/mysqldemo/mapper/StudentMapper.java
+++ src/main/java/com/example/mysqldemo/mapper/StudentMapper.java
@@ -3,17 +3,8 @@ package com.example.mysqldemo.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.example.mysqldemo.entity.Student;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.ResultMap;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface StudentMapper extends BaseMapper<Student> {
-
-
-    // 查询学生的所有课程成绩
-    @ResultMap("studentMap")
-    @Select("select * from student where id = #{id}")
-    Object selectStudentCourse(Integer id);
-
 }
 

8fe6653b7f0ac59f1e9fb3caeec3c84150056789
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-12-14 19:24:31 +0800

    新增接口

--- src/main/java/com/example/mysqldemo/service/TeacherService.java
+++ src/main/java/com/example/mysqldemo/service/TeacherService.java
@@ -2,6 +2,7 @@ package com.example.mysqldemo.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.mysqldemo.entity.Teacher;
+import org.springframework.stereotype.Service;
 
 public interface TeacherService extends IService<Teacher> {
 

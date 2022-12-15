313a402097f3cee2faae784eda032e4556a39671
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-12-16 02:59:24 +0800

    fix：修复部分bug

--- src/main/java/com/example/mysqldemo/mapper/ScoreMapper.java
+++ src/main/java/com/example/mysqldemo/mapper/ScoreMapper.java
@@ -12,7 +12,7 @@ import java.util.List;
 public interface ScoreMapper extends BaseMapper<Score> {
 
     // 从Student表中查询学生的所有成绩
-    @Select("SELECT student.school_id,student.student_name,course_name,score,score.course_id,teacher_name,course_id,score.id id FROM student,course,score,teacher WHERE student_id=student.id AND course.id=score.course_id AND course.teacher_id=teacher.id; WHERE student.student_name like CONCAT('%',#{studentName},'%');")
+    @Select("SELECT student.school_id,student.student_name,course_name,score,score.course_id,teacher_name,course_id,score.id id FROM student,course,score,teacher WHERE student_id=student.id AND course.id=score.course_id AND course.teacher_id=teacher.id AND student.student_name like CONCAT('%',#{studentName},'%');")
     List<ScoreDto> getScoreByStudentName(String studentName);
 
     @Select("SELECT student.school_id,student.student_name,course_name,score,score.course_id,teacher_name,course_id,score.id id FROM student,course,score,teacher WHERE student_id=student.id AND course.id=score.course_id AND course.teacher_id=teacher.id;")

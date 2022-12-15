47755c1c890237947ecf141a51b9b85e904f8a9f
LAPTOP-VMIJDT2P\li hanyu <1328411791@qq.com>
2022-12-16 02:57:24 +0800

    fix：修复部分bug

--- src/test/java/com/example/mysqldemo/StudentTests.java
+++ src/test/java/com/example/mysqldemo/StudentTests.java
@@ -1,6 +1,7 @@
 package com.example.mysqldemo;
 
 import com.example.mysqldemo.common.Result;
+import com.example.mysqldemo.service.ScoreService;
 import com.example.mysqldemo.service.StudentService;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +14,14 @@ public class StudentTests {
     @Resource
     private StudentService studentService;
 
+    @Resource
+    private ScoreService scoreService;
+
     @Test
     public void testSelectStudentName() {
         Result result = studentService.selectStudentName("张");
         System.out.println(result);
+        Result result1 = scoreService.getAllStudentScore();
+        System.out.println(result1);
     }
 }
